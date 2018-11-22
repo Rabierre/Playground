4badb41ae1b3e7563b76c7804b937e1936d4dbcf
rabierre <rabierre@gmail.com>
2018-11-22 17:06:44 +0900

    Add build script

--- pages/notion.js
+++ pages/notion.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 // import {Card, Grid, Button} from 'semantic-ui-react';
-import {Container,Grid, Segment, Button, Card, Image } from 'semantic-ui-react';
+// import {Container,Grid, Segment, Button, Card, Image } from 'semantic-ui-react';
 import Layout from '../components/Layout';
-import TicketList from '../components/TicketList';
-import axios from 'axios';
-import Iframe from 'react-iframe'
+// import TicketList from '../components/TicketList';
+// import axios from 'axios';
+// import Iframe from 'react-iframe'
 
 
 class Notion extends Component {
